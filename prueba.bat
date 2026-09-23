5dbb7ecb562c29ade8daab56bcf5b1cbb6cda2be
Deohvan Aguilar <aguilar.ramirez.deohvanp11@gmail.com>
2026-09-22 18:30:36 -0700

    Carpeta views

---
+++ prueba.bat
@@ -0,0 +1,10 @@
+@ echo off
+title ejemplo de archivo de procesamiento por lotes 
+echo bienvenido al mundo de los archivos .bat
+pause
+
+ @echo off
+ echo creando un archivo > nuevo.txt
+ echo Esta es la Segunda línea >> nuevo.txt 
+ echo Y esta es la Siguiente >> nuevo.txt  
+ 
